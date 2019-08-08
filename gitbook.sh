1c21ad3416f1714ea33d5582e299f1ee8d99d5ab
liu_jing <liu_jing5@hoperun.com>
2019-08-08 13:55:22 +0800

    测试提交1355

--- gitbook.sh
+++ gitbook.sh
@@ -1,7 +1,9 @@
+mv _book/* ../liujingCode/
+rm -r _book
 git add .
 git commit -m $1
-git push -u origin master
-cp _book/* ./Blog
+git push origin master
+cd ../liujingCode
 git add .
 git commit -m $1
-git push -u origin master
+git push origin master
